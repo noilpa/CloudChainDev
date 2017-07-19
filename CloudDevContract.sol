acc9b7b762d342eadb6e4de1131d031e907cbee6
NovikovIP <ilyanovikov1994@yandex.ru>
2017-07-19 22:01:05 +0300

    add CloudChainDev.sol function implementation

--- CloudDevContract.sol
+++ CloudDevContract.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.4.11;
 contract CloudICO {
 
     struct ProjectPassport {
-        string summary;                         // краткое описание проета
-        string description;                     // полное описание проекта
-        uint price;                             // цена, за которую работа будет сделана
-        uint minCashPart;                       // минимальный порог для финансирования
-        uint minCashVoting;                     // минимальный порог для участия в голосовании
-        string dueDate;                         // срок выполнения проекта
-        bytes32 filesHash;                      // хэш файлов которые приложил 
-        mapping (address => Shareholder) users; // акционеры проекта
+        string summary;                                             // краткое описание проета
+        string description;                                         // полное описание проекта
+        string edits;                                               // описание правок проекта
+        uint price;                                                 // цена, за которую работа будет сделана
+        uint minCashPart;                                           // минимальный порог для финансирования
+        uint minCashVoting;                                         // минимальный порог для участия в голосовании
+        string dueDate;                                             // срок выполнения проекта
+        bytes32 filesHash;                                          // хэш файлов которые приложил 
+        mapping (string => mapping (address => Shareholder)) users;  // акционеры проекта
     }
 
     struct Shareholder {
@@ -23,18 +24,35 @@ contract CloudICO {
 
     // Занесение паспорта проекта в БЧ
     function SetProjectPassport(string _summary, string _description, uint _price, string _dueDate) {
-        
+
+        project.summary = _summary;
+        project.description = _description;
+        project.dueDate = _dueDate;
+        project.price = _price;
+        project.minCashPart = _price * 0.05;    // Коэффициент 0.05 условный
+        project.minCashVoting = _price * 0.1;   // Коэффициент 0.1 условный
     }
 
     // Добавление акционера к проекту 
-    function AddShareholder(address _address, uint _cash, uint _status) {
-        
+    function AddShareholder(string _user, address _address, uint _cash, uint _status) {
+
+        project.users[_user][_address].cash = _cash;
+        project.users[_user][_address].status = _status;
     }
 
-    // Получение статуса проекта: собранная сумма, участники, стоимость проекта, актуальное ТЗ (description?)
+    // Получение статуса проекта: собранная сумма, участники, стоимость проекта, правки
+    // Вопрос как венуть mapping?
     function GetProjectStatus() returns() {
 
+        return (
+
+                );
     }
 
+    // Добоваление правок в проект
+    function SetEdits(string _edits) {
+
+        project.edits = _edits;
+    }
 
 }
